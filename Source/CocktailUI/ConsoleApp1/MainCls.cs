83d347a98c4e902dbcc814477b098c859bd2bcf5
vishipayyallore <vishipayyallore@yahoo.com>
2016-01-24 23:53:07 +0530

    A

---
+++ Source/CocktailUI/ConsoleApp1/MainCls.cs
@@ -0,0 +1,18 @@
+﻿using static System.Console;
+using FirstLibrary;
+
+namespace ConsoleApp1
+{
+
+    public class MainCls
+    {
+        public static void Main(string[] args)
+        {
+            var panda1 = new Panda("Pan Dee");
+            var panda2 = new Panda("Pan Dah");
+            WriteLine($"{panda1} {panda2} \n\tTotal: {Panda.Population}");
+            ReadKey();
+        }
+    }
+
+}
