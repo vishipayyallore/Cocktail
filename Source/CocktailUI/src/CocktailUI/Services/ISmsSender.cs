9fbd68c6c66e7102ca793c0753d5fe82466e1768
vishipayyallore <vishipayyallore@yahoo.com>
2016-01-26 11:12:37 +0530

    Initial Version

---
+++ Source/CocktailUI/src/CocktailUI/Services/ISmsSender.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace CocktailUI.Services
+{
+    public interface ISmsSender
+    {
+        Task SendSmsAsync(string number, string message);
+    }
+}
---
+++ Source/CocktailUI/src/CocktailUI/ViewModels/Manage/FactorViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace CocktailUI.ViewModels.Manage
+{
+    public class FactorViewModel
+    {
+        public string Purpose { get; set; }
+    }
+}
